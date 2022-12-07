5601f3b766bef21940f13161a9c9a5e3eba4b712
Leva <parimalleva@gmail.com>
2022-12-07 22:09:23 +0300

    finish hw03-m03

--- hw03-m03/src/app/task1/task1.component.ts
+++ hw03-m03/src/app/task1/task1.component.ts
@@ -7,4 +7,61 @@ import { Component } from '@angular/core';
 })
 export class Task1Component {
 
+  titleProgrammingLanguage: string = "Информация об понимании языков программирования в %";
+  titleSalariesProgrammingLanguage: string = "Информация об зарплатах по языкам программированя в";
+  sizeProgrammingSalary!: number;
+
+  programmingLanguage = [
+    {
+      type: "C",
+      undefined: 0.58
+    },
+    {
+      type: "C++",
+      undefined: 0.63
+    },
+    {
+      type: "Java",
+      undefined: 0.76
+    },
+    {
+      type: "JavaScript",
+      undefined: 0.67
+    },
+    {
+      type: "TypeScript",
+      undefined: 0.38
+    },
+  ];
+
+  programmingSalary = [
+    {
+      type: "C++",
+      salaryMin: 62599,
+      salaryMax: 86172
+    },
+    {
+      type: "Java",
+      salaryMin: 70187,
+      salaryMax: 123554
+    },
+    {
+      type: "JavaScript",
+      salaryMin: 97577,
+      salaryMax: 119839
+    },
+
+  ];
+
+  getSizeProgrammingSalary(programmingSalary: []){
+
+  }
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
 }
--- hw03-m03/src/app/task2/task2.component.ts
+++ hw03-m03/src/app/task2/task2.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class Task2Component {
 
+  titleEducation: string = "Информация об образовании";
+
+  education = [
+    {
+      dateStart: new Date(1991, 0),
+      dateFinish: new Date(2000, 0),
+      type: "Среднее образование",
+      place: "Гомельская средняя школа №25"
+    },
+    {
+      dateStart: new Date(2000, 0),
+      dateFinish: new Date(2003, 0),
+      type: "Среднее техническое образование",
+      place: "Гомельское профессионально-техническое училище №78 машиностроения"
+    },
+    {
+      dateStart: new Date(2003, 0),
+      dateFinish: new Date(2008, 0),
+      type: "Высшее образование",
+      place: "Белорусский Государственный Университет Tранспорта"
+    },
+
+  ];
+
 }
